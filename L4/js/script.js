0ee6eee6f7d99299d92596b6189f722c64350f29
Felix Sopelsa <felix.sopelsa@gmail.com>
2022-02-24 20:10:54 +0100

    klonat L4-mappen för att ha kvar grund-koden, samt lagt in kommentarer från instruktionsvideon för L4

--- L4/js/script.js
+++ L4/js/script.js
@@ -1,11 +1,11 @@
 // Globala variabler
-var titleElem;		// Referens till element för bildspelets titel
-var imgElem;		// Referens till img-element för bildspelet
-var captionElem;	// Referens till element för bildtext
-var imgUrls;		// Array med url:er för valda bilder
-var imgCaptions;	// Array med bildtexter till valda bilder
-var imgIx;			// Index för aktuell bild
-var timer;			// Referens till timern för bildspelet
+	// Referens till element för bildspelets titel
+	// Referens till img-element för bildspelet
+	// Referens till element för bildtext
+	// Array med url:er för valda bilder
+	// Array med bildtexter till valda bilder
+	// Index för aktuell bild
+	// Referens till timern för bildspelet
 
 // Initiering av globala variabler och händelsehanterare
 function init() {
@@ -22,25 +22,32 @@ function init() {
 				this.selectedIndex = 0;
 			}
 		);
+ //-------------GÖR OM SÅ REFERERAR TILL METODERNA ISTÄLLET FÖR GAMLA FUNKTIONER------------\\
+//-------------"Använd variabeln för instansen, för att referera till objektet."-------------\\
 	document.querySelector("#prevBtn").addEventListener("click",prevImage);
 	document.querySelector("#nextBtn").addEventListener("click",nextImage);
 	
-	// ----- Extramerit -----
-	/* document.querySelector("#autoBtn").addEventListener("click",
-			function(e) {
-				autoImage(e,3000);
-			}
-		);
-	*/
-
-	// ----- Guldstjärna -----
-	//		Här ska du lägga kod, om du gör guldstjärneuppgiften
+	// let self = this
 	
 } // End init
 window.addEventListener("load",init);
 
 // ---------------------------------------------------------------
-// ----- Funktioner för bildspelet -----
+// ----- Funktioner/metoder för bildspelet -----    			  -_-OBJEKTET-_-
+//																				\\
+function constructorFunction() {
+	// Gör om till egenskaper	-	Flytta hit det som behövs från init
+	titleElem;		// Referens till element för bildspelets titel
+	imgElem;		// Referens till img-element för bildspelet
+	captionElem;	// Referens till element för bildtext
+	imgUrls;		// Array med url:er för valda bilder
+	imgCaptions;	// Array med bildtexter till valda bilder
+	imgIx;			// Index för aktuell bild
+	timer;			// Referens till timern för bildspelet
+
+}
+
+         //Skriv om så att de blir metoder i objektet 
 
 // Gör ett Ajax-anrop för att läsa in begärd fil
 function requestImages(file) { // Parametern nr används i url:en för den fil som ska läsas in
@@ -53,6 +60,9 @@ function requestImages(file) { // Parametern nr används i url:en för den fil s
 			else document.getElementById("result").innerHTML = "Den begärda resursen fanns inte.";
 	};
 } // End requestImages
+
+
+         //Skriv om så att de blir metoder i objektet 
 
 // Funktion för att tolka XML-koden och lägga in innehållet i variablerna för bilderna i bildspelet
 function getImages(XMLcode) { // Parametern XMLcode är hela den inlästa XML-koden
@@ -70,37 +80,21 @@ function getImages(XMLcode) { // Parametern XMLcode är hela den inlästa XML-ko
 } // End getImages
 
 // Visa bilden med index imgIx
-function showImage() {
+function showImage() {         //Skriv om så att de blir metoder i objektet 
 	imgElem.src = imgUrls[imgIx];
 	captionElem.innerHTML = (imgIx+1) + ". " + imgCaptions[imgIx];
 } // End showImage
 
 // Visa föregående bild
-function prevImage() {
+function prevImage() {         //Skriv om så att de blir metoder i objektet 
 	if (imgIx > 0) imgIx--;
 	else imgIx = imgUrls.length - 1; // Gå runt till sista bilden
 	showImage();
 } // End prevImage
 
 // Visa nästa bild
-function nextImage() {
+function nextImage() {         //Skriv om så att de blir metoder i objektet 
 	if (imgIx < imgUrls.length - 1) imgIx++;
 	else imgIx = 0; // Gå runt till första bilden
 	showImage();
 } // End nextImage
-
-// ----- Extramerit -----
-/* Ta bort kommentaren kring koden, för att testa funktionaliteten för extrameriten
-// Starta/stoppa automatisk bildvisning
-function autoImage(e,interval) {
-	if (timer == null) { // Start
-		timer = setInterval(nextImage,interval);
-		if (e) e.currentTarget.style.backgroundColor = "green";
-	}
-	else { // Stopp
-		clearInterval(timer);
-		timer = null;
-		if (e) e.currentTarget.style.backgroundColor = "white";
-	}
-} // End autoImage
-*/
