347eecae412d6715a901a78fea6442103bb6dd9f
Felix Sopelsa <felix.sopelsa@gmail.com>
2022-04-15 01:27:10 +0200

    L5b klar

--- L5/L5b/js/script.js
+++ L5/L5b/js/script.js
@@ -12,54 +12,63 @@ var platformElem;		//Referens till där platformar syns
 var moreinfoElem;		//Referens till där länk syns
 var imgIx;				//Referens till vilken bild som klickas på
 //________________________________________________________________________________________________________________________\\
+// Initiering av lokal variabel "gallery" samt händelsehanterare
 function init() {
-	infoElems = document.getElementById("infoElems").querySelectorAll("p");
-	titleElem = document.getElementById("title");
-	genreElem = document.getElementById("genre");
-	yearElem = document.getElementById("year");
-	developerElem = document.getElementById("developer");
-	metascoreElem = document.getElementById("metascore");
-	playerVersus = document.getElementById("playerVersus");
-	numberOfPlayers = document.getElementById("numberOfPlayers");
-	numberOfLives = document.getElementById("numberOfLives");
-	platformElem = document.getElementById("platform");
-	moreinfoElem = document.getElementById("moreinfo");
-	urlElem = document.getElementById("url");
+	let gallery = new ImageViewer("imgViewer");				// Lokal variabel som objektets instans sparas i
 	let imgElems = document.querySelectorAll("main div:first-of-type a");
 	for (let i = 0; i < imgElems.length; i++) {
-		imgElems[i].addEventListener("click",requestJSON); // Klick gör att hela JSON-filen hämtas
+		imgElems[i].addEventListener("click",function() { gallery.requestJSON(this.id); }); // Klick gör att hela JSON-filen hämtas
 		imgElems[i].style.cursor = "pointer";
 	}
 } // End init
 window.addEventListener("load",init);
+
+  //--------------------------------------------------------------------------------------------------------------------\\
+ //____________________________________________---/\OBJEKTET ImageViewer/\---____________________________________________\\
+//-Constructor för objektet ImageViewer______________________________________________-Constructor för objektet ImageViewer\\
+function ImageViewer(id) {
+	this.infoElems = document.getElementById("infoElems").querySelectorAll("p");
+	this.titleElem = document.getElementById("title");
+	this.genreElem = document.getElementById("genre");
+	this.yearElem = document.getElementById("year");
+	this.developerElem = document.getElementById("developer");
+	this.metascoreElem = document.getElementById("metascore");
+	this.playerVersus = document.getElementById("playerVersus");
+	this.numberOfPlayers = document.getElementById("numberOfPlayers");
+	this.numberOfLives = document.getElementById("numberOfLives");
+	this.platformElem = document.getElementById("platform");
+	this.moreinfoElem = document.getElementById("moreinfo");
+	this.urlElem = document.getElementById("url");
+} // End ImageViewer
  //________________________________________________________________________________________________________________________\\
 //-Gör ett Ajax-anrop för att läsa in begärd fil______________________________-Gör ett Ajax-anrop för att läsa in begärd fil\\
-function requestJSON () { // Parametern nr används i url:en för den fil som ska läsas in
-	imgIx = this.id;      // Sparar vilken bild som är klickad på i variabeln imgIx
+ImageViewer.prototype.requestJSON = function(imgId) { // Parametern nr används i url:en för den fil som ska läsas in
+	let self = this;		// Sparar this i en lokal variabel så att den kan användas för metodsanrop längre ned
+	imgIx = imgId;      // Sparar vilken bild som är klickad på i variabeln imgIx
 	let request = new XMLHttpRequest(); // Object för Ajax-anropet
 	request.open("GET","json/storyDriven.json",true);
 	request.send(null); // Skicka begäran till servern
 	request.onreadystatechange = function () { // Funktion för att avläsa status i kommunikationen
 		if (request.readyState == 4) // readyState 4 --> kommunikationen är klar
-			if (request.status == 200) showInfo(request.responseText, imgIx); // status 200 (OK) --> filen fanns
+			if (request.status == 200) self.showInfo(request.responseText, imgIx); // status 200 (OK) --> filen fanns
 			else document.getElementById("infoElems").innerHTML = "Den begärda resursen fanns inte.";
 	};
 } // End requestImages
  //________________________________________________________________________________________________________________________\\
 //-Tolka XML-koden från den begärda filen___________________________________________-Tolka JSON-koden från den begärda filen\\
-function showInfo(JSONtext, imgIx) { 		   // Parametern JSONtext är hela den inlästa JSON-koden
+ImageViewer.prototype.showInfo = function(JSONtext, imgIx) { 		   // Parametern JSONtext är hela den inlästa JSON-koden
 	let games = JSON.parse(JSONtext).game;			  	  	 // Lokal variabel där vald JSON-info sparas
-	clickedGame = games[imgIx];
-	titleElem.innerHTML =  "  " + clickedGame.title; 						// Titel läggs in
-	genreElem.innerHTML = " " + clickedGame.genre; 							// Genre läggs in
-	yearElem.innerHTML = " " + clickedGame.year; 							// Utgivningsår läggs in
-	developerElem.innerHTML = " " + clickedGame.developer; 					// Utveklare läggs in
-	metascoreElem.innerHTML = " " + clickedGame.metascore; 					// Metascore läggs in
-	platformElem.innerHTML = " " + clickedGame.platform; 					// Platformar läggs in
-	playerVersus.innerHTML = " " + clickedGame.rules.playerVersus; 			// Versus läggs in
-	numberOfPlayers.innerHTML = " " + clickedGame.rules.numberOfPlayers; 	// Antal spelare läggs in
-	numberOfLives.innerHTML =  " " + clickedGame.rules.numberOfLives; 		// Antal liv läggs in
-	moreinfoElem.innerHTML = " " + clickedGame.moreinfo; 					// Länk läggs in
-	moreinfoElem.setAttribute('href', clickedGame.moreinfo); 				// Länk görs klickbar
+	let clickedGame = games[imgIx];
+	this.titleElem.innerHTML =  "  " + clickedGame.title; 						// Titel läggs in
+	this.genreElem.innerHTML = " " + clickedGame.genre; 							// Genre läggs in
+	this.yearElem.innerHTML = " " + clickedGame.year; 							// Utgivningsår läggs in
+	this.developerElem.innerHTML = " " + clickedGame.developer; 					// Utveklare läggs in
+	this.metascoreElem.innerHTML = " " + clickedGame.metascore; 					// Metascore läggs in
+	this.platformElem.innerHTML = " " + clickedGame.platform; 					// Platformar läggs in
+	this.playerVersus.innerHTML = " " + clickedGame.rules.playerVersus; 			// Versus läggs in
+	this.numberOfPlayers.innerHTML = " " + clickedGame.rules.numberOfPlayers; 	// Antal spelare läggs in
+	this.numberOfLives.innerHTML =  " " + clickedGame.rules.numberOfLives; 		// Antal liv läggs in
+	this.moreinfoElem.innerHTML = " " + clickedGame.moreinfo; 					// Länk läggs in
+	this.moreinfoElem.setAttribute('href', clickedGame.moreinfo); 				// Länk görs klickbar
 } // End getImages
  //________________________________________________________________________________________________________________________\\
